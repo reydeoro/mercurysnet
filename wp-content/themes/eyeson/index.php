a0e27d86d04d2a1a30f0ccfa234a3da82a2e1141
reydeoro <senaotantanmen@gmail.com>
2022-02-07 00:55:50 +0900

    タイトルの文字表示部分を変更。色合いをつけるようにした。

--- wp-content/themes/eyeson/index.php
+++ wp-content/themes/eyeson/index.php
@@ -20,6 +20,29 @@
  				<br>
 				みんなで楽しめる、楽しませるやばい吟遊詩人で〜す！
  			</p>
+	
+		<!--最新ニュースを表示します-->
+		<section id="thenews">
+			<div class="newstitle">
+				柚原からのお知らせ！
+			</div>
+			<div class="topics">
+				<dl>
+					<dt>20180101</dt>
+					<dd>新着情報の内容が入ります。</dd>
+				</dl>
+
+				<dl>
+					<dt>20180101</dt>
+					<dd>新着情報の内容が入ります。</dd>
+				</dl>
+
+				<dl>
+					<dt>20180101</dt>
+					<dd>新着情報の内容が入ります。</dd>
+				</dl>
+			</div>
+		</section>
 
 	 <section id="content">
 	 <div id="content-wrap" class="container">
@@ -47,12 +70,49 @@
 		<!--メイン記事をかき込みます-->
 	 	<div id="main" class="col-md-9">
 			<section id="tubetube">
-				<h2>・YouTubeチャンネル</h2>
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/feSC-1wSngM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<div class="newsbodytitle">
+				・Youtubeチャンネル
+			</div>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/feSC-1wSngM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</section>
 
+			<!--最新ニュースを表示-->
 			<section id="newsnews">
-				<h2>・最新記事</h2>
+				<div class="newsbodytitle">
+				・最新記事
+				</div>
+			<!--おすすめ記事の表示、4件をflexで表示したい-->
+			<div class="newart">
+				<div class="newartitem">
+					<img src="img/movie.jpg">
+					<h2>最新記事</h2>
+					<p>最新記事をここに載っけます</p>
+				</div>
+
+				<div class="newartitem">
+					<img src="img/movie.jpg">
+					<h2>最新動画</h2>
+					<p>最新動画をここに載っけます</p>
+				</div>
+
+				<div class="newartitem">
+					<img src="img/movie.jpg">
+					<h2>競馬予想</h2>
+					<p>競馬予想をここに載っけます</p>
+				</div>
+
+				<div class="newartitem">
+					<img src="img/movie.jpg">
+					<h2>競馬予想</h2>
+					<p>競馬予想をここに載っけます</p>
+				</div>
+			</div>
+
+			<!--柚原のブログを紹介するところです。ぺージャーを追加予定-->
+			<section id="yuzublog">
+			<div class="newsbodytitle">
+				柚原からのお知らせ！
+			</div>
 			<!--おすすめ記事の表示、4件をflexで表示したい-->
 			<div class="newart">
 				<div class="newartitem">
